1831d85eb3028d3ec320df950b48f1bdfec18285
RogelioRamos <rogelio.ramosch@gmail.com>
2018-12-20 11:22:10 -0600

    Arreglado el color del Log In

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,14 +10,14 @@ class Login extends Component
                 <div id="logincillo" className="login-container">
                 <section className="login" id="login">
                     <header>
-                    <h2>Application Name</h2>
-                    <h4>Login</h4>
+                    <h2>Introduce tus datos</h2>
+                    <h4>Clizza</h4>
                     </header>
                     <form className="login-form" action="#" method="post">
                     <input type="text" className="login-input" placeholder="User" required autoFocus/>
                     <input type="password" className="login-input" placeholder="Password" required/>
                     <div className="submit-container">
-                        <button type="submit" className="login-button">SIGN IN</button>
+                        <button type="submit" className="login-button">Inicia Sesión</button>
                     </div>
                     </form>
                 </section>
